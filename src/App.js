d1dac69f4e3b02de948d88cbec3e351a303b0a53
friansh <fikriultimate18@gmail.com>
2020-03-23 23:12:25 +0700

    Fitur covid sama footer

--- src/App.js
+++ src/App.js
@@ -34,64 +34,94 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Navbar
-          bg="light"
-          expand="md"
-          expanded={this.state.expanded}
-          onToggle={this.handleNavToggle}
-        >
-          <Navbar.Brand href="/">SIAPMEN</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Container fluid>
+        <Container fluid>
+          <Row>
+            <Navbar
+              bg="light"
+              expand="md"
+              expanded={this.state.expanded}
+              onToggle={this.handleNavToggle}
+            >
+              <Navbar.Brand href="/">SIAPMEN</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Container fluid>
+                  <Row>
+                    <Col md="auto" style={{ paddingLeft: "10px" }}>
+                      <Link
+                        to="/"
+                        style={{ textDecoration: "inherit", color: "inherit" }}
+                        onClick={this.shrinkNav}
+                      >
+                        Home
+                      </Link>
+                    </Col>
+                    <Col md="auto" style={{ paddingLeft: "10px" }}>
+                      <Link
+                        to="/add"
+                        style={{ textDecoration: "inherit", color: "inherit" }}
+                        onClick={this.shrinkNav}
+                      >
+                        Request
+                      </Link>
+                    </Col>
+                    <Col md="auto" style={{ paddingLeft: "10px" }}>
+                      <Link
+                        to="/edit"
+                        style={{ textDecoration: "inherit", color: "inherit" }}
+                        onClick={this.shrinkNav}
+                      >
+                        Status
+                      </Link>
+                    </Col>
+                  </Row>
+                </Container>
+              </Navbar.Collapse>
+            </Navbar>
+          </Row>
+          <Row>
+            <Container
+              fluid
+              style={{ backgroundColor: "#a4b0be", minHeight: "100vh" }}
+              className={"pt-4 pb-5"}
+            >
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/add">
+                  <Add />
+                </Route>
+                <Route path="/edit">
+                  <Edit />
+                </Route>
+              </Switch>
+            </Container>
+          </Row>
+          <Row
+            style={{
+              minWidth: "100vh",
+              minHeight: "90px",
+              backgroundColor: "#212121"
+            }}
+          >
+            <Container>
               <Row>
-                <Col md="auto" style={{ paddingLeft: "10px" }}>
-                  <Link
-                    to="/"
-                    style={{ textDecoration: "inherit", color: "inherit" }}
-                    onClick={this.shrinkNav}
-                  >
-                    Home
-                  </Link>
+                <Col className="pt-4">
+                  <small className="text-light">
+                    Copyright &copy; 2020 Fikri Rida Pebriansyah
+                    <br />
+                    Departemen Akademik dan Inovasi
+                  </small>
                 </Col>
-                <Col md="auto" style={{ paddingLeft: "10px" }}>
-                  <Link
-                    to="/add"
-                    style={{ textDecoration: "inherit", color: "inherit" }}
-                    onClick={this.shrinkNav}
-                  >
-                    Request
-                  </Link>
-                </Col>
-                <Col md="auto" style={{ paddingLeft: "10px" }}>
-                  <Link
-                    to="/edit"
-                    style={{ textDecoration: "inherit", color: "inherit" }}
-                    onClick={this.shrinkNav}
-                  >
-                    Status
-                  </Link>
+                <Col className="py-3 justify-content-end d-flex">
+                  <img src="logo/unpad.png" height="60" alt="unpad" />
+                  <img src="logo/bem.png" height="60" alt="bem" />
+                  <img src="logo/xman.png" height="60" alt="xman" />
                 </Col>
               </Row>
             </Container>
-          </Navbar.Collapse>
-        </Navbar>
-        <Container
-          fluid
-          style={{ backgroundColor: "#a4b0be", minHeight: "100vh" }}
-          className={"pt-4 pb-5"}
-        >
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/add">
-              <Add />
-            </Route>
-            <Route path="/edit">
-              <Edit />
-            </Route>
-          </Switch>
+          </Row>
         </Container>
       </BrowserRouter>
     );
--- src/App/HomeApp.js
+++ src/App/HomeApp.js
@@ -6,14 +6,18 @@ import {
   Jumbotron,
   Carousel,
   Button,
-  Modal
+  Modal,
+  Card
 } from "react-bootstrap";
+import Axios from "axios";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      SOP: false
+      SOP: false,
+      loading: true,
+      coronaData: {}
     };
     this.handleSOPModalShow = this.handleSOPModalShow.bind(this);
     this.handleSOPModalHide = this.handleSOPModalHide.bind(this);
@@ -31,87 +35,202 @@ class Home extends React.Component {
     });
   }
 
+  componentDidMount() {
+    let self = this;
+    Axios.get("https://api.u9.nu/covid19")
+      .then(response =>
+        self.setState({
+          coronaData: response.data
+        })
+      )
+      .then(() => {
+        console.log(this.state.coronaData);
+        self.setState({
+          loading: false
+        });
+        console.log(this.state.coronaData.nasional.slice(-1)[0]);
+      });
+  }
+
   render() {
-    return (
-      <Container>
-        <Row>
-          <Col lg="8">
-            <Jumbotron>
-              <h1>Halo dunia!</h1>
-              <p>
-                Selamat datang di Sistem Antrean Publikasi Medfo Akinov
-                <b> (SIAPMEN)</b>. <br />
-                Silahkan buka tab{" "}
-                <b>
-                  <i>Request</i>
-                </b>{" "}
-                untuk menambahkan permintaan publikasi dan buka tab{" "}
-                <b>
-                  <i>Status</i>
-                </b>{" "}
-                untuk melihat perkembangan proses pembuatan publikasi.
+    if (!this.state.loading) {
+      return (
+        <Container className="mb-3">
+          <Row>
+            <Col>
+              <h1
+                style={{
+                  color: "white",
+                  textShadow: "1px 2px 3px rgba(0,0,0,0.43)"
+                }}
+              >
+                Info Covid19 hari ini
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="3" className="mb-3 mb-md-0">
+              <Card body style={{ backgroundColor: "#ffe196" }}>
+                <h5>Positif</h5>
+                <h2>
+                  {
+                    this.state.coronaData.nasional.slice(-1)[0]
+                      .positif_kumulatif
+                  }
+                </h2>
+              </Card>
+            </Col>
+            <Col md="3" className="mb-3 mb-md-0">
+              <Card body style={{ backgroundColor: "#d8b5b5" }}>
+                <h5>Dalam Perawatan</h5>
+                <h2>{this.state.coronaData.nasional.slice(-1)[0].perawatan}</h2>
+                <p>
+                  {this.state.coronaData.nasional
+                    .slice(-1)[0]
+                    .presentase_perawatan.toString()
+                    .slice(0, 2) +
+                    "." +
+                    this.state.coronaData.nasional
+                      .slice(-1)[0]
+                      .presentase_perawatan.toString()
+                      .slice(2)}
+                  % dari terkonfirmasi
+                </p>
+              </Card>
+            </Col>
+            <Col md="3" className="mb-3 mb-md-0">
+              <Card body style={{ backgroundColor: "#05dfd7" }}>
+                <h5>Sembuh</h5>
+                <h2>{this.state.coronaData.nasional.slice(-1)[0].sembuh}</h2>
+                <p>
+                  {this.state.coronaData.nasional
+                    .slice(-1)[0]
+                    .presentase_sembuh.toString()
+                    .slice(0, 2) +
+                    "." +
+                    this.state.coronaData.nasional
+                      .slice(-1)[0]
+                      .presentase_sembuh.toString()
+                      .slice(2)}
+                  % dari terkonfirmasi
+                </p>
+              </Card>
+            </Col>
+            <Col md="3" className="mb-3 mb-md-0">
+              <Card body style={{ backgroundColor: "#ec7373" }}>
+                <h5>Meninggal</h5>
+                <h2>{this.state.coronaData.nasional.slice(-1)[0].meninggal}</h2>
+                <p>
+                  {this.state.coronaData.nasional
+                    .slice(-1)[0]
+                    .presentase_meninggal.toString()
+                    .slice(0, 2) +
+                    "." +
+                    this.state.coronaData.nasional
+                      .slice(-1)[0]
+                      .presentase_meninggal.toString()
+                      .slice(2)}
+                  % dari terkonfirmasi
+                </p>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p
+                className="text-right"
+                style={{
+                  color: "white",
+                  textShadow: "1px 2px 3px rgba(0,0,0,0.43)"
+                }}
+              >
+                Sumber: covid19.go.id
               </p>
               <hr />
-              <p>Jangan lupa bahagia hari ini ;)</p>
-              <Button variant="primary" onClick={this.handleSOPModalShow}>
-                SOP Kami
-              </Button>
-              <Modal
-                show={this.state.SOP}
-                onHide={this.handleSOPModalHide}
-                centered
-              >
-                <Modal.Header closeButton>
-                  <Modal.Title>SOP Kami</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <p>
-                    <b>1.</b> Poster paling lambat <b>H-3</b> dari waktu
-                    publikasi
-                    <br />
-                    <b>2.</b> Video paling lambat <b>H-7</b> dari waktu
-                    publikasi
-                    <br />
-                    <b>3.</b> Poster dan Video paling lambat <b>H-8</b> dari
-                    waktu publikasi
-                    <br />
-                    <b>4.</b> Live Report paling lambat <b>H-1</b> dari waktu
-                    publikasi
-                    <br />
-                    <b>5.</b> Repost BEM Fakultas paling lambat <b>H-2</b> dari
-                    waktu publikasi
-                    <br />
-                    <b>6.</b> Repost Eksternal Lainnya paling lambat <b>H-2</b>{" "}
-                    dari waktu publikasi dihitung saat sudah memberikan feedback
-                    <br />
-                    <br />
-                    <b>Mengorder berarti menyetujui sist.</b>
-                  </p>
-                </Modal.Body>
-              </Modal>
-            </Jumbotron>
-          </Col>
-          <Col lg="4">
-            <Carousel>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="./carousel/1.jpg"
-                  alt="Akinov"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="./carousel/2.jpg"
-                  alt="Medfo"
-                />
-              </Carousel.Item>
-            </Carousel>
-          </Col>
-        </Row>
-      </Container>
-    );
+            </Col>
+          </Row>
+          <Row>
+            <Col lg="8">
+              <Jumbotron>
+                <h1>Halo dunia!</h1>
+                <p>
+                  Selamat datang di Sistem Antrean Publikasi Medfo Akinov
+                  <b> (SIAPMEN)</b>. <br />
+                  Silahkan buka tab{" "}
+                  <b>
+                    <i>Request</i>
+                  </b>{" "}
+                  untuk menambahkan permintaan publikasi dan buka tab{" "}
+                  <b>
+                    <i>Status</i>
+                  </b>{" "}
+                  untuk melihat perkembangan proses pembuatan publikasi.
+                </p>
+                <hr />
+                <p>Jangan lupa bahagia hari ini ;)</p>
+                <Button variant="primary" onClick={this.handleSOPModalShow}>
+                  SOP Kami
+                </Button>
+                <Modal
+                  show={this.state.SOP}
+                  onHide={this.handleSOPModalHide}
+                  centered
+                >
+                  <Modal.Header closeButton>
+                    <Modal.Title>SOP Kami</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <p>
+                      <b>1.</b> Poster paling lambat <b>H-3</b> dari waktu
+                      publikasi
+                      <br />
+                      <b>2.</b> Video paling lambat <b>H-7</b> dari waktu
+                      publikasi
+                      <br />
+                      <b>3.</b> Poster dan Video paling lambat <b>H-8</b> dari
+                      waktu publikasi
+                      <br />
+                      <b>4.</b> Live Report paling lambat <b>H-1</b> dari waktu
+                      publikasi
+                      <br />
+                      <b>5.</b> Repost BEM Fakultas paling lambat <b>H-2</b>{" "}
+                      dari waktu publikasi
+                      <br />
+                      <b>6.</b> Repost Eksternal Lainnya paling lambat{" "}
+                      <b>H-2</b> dari waktu publikasi dihitung saat sudah
+                      memberikan feedback
+                      <br />
+                      <br />
+                      <b>Mengorder berarti menyetujui sist.</b>
+                    </p>
+                  </Modal.Body>
+                </Modal>
+              </Jumbotron>
+            </Col>
+            <Col lg="4">
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="./carousel/1.jpg"
+                    alt="Akinov"
+                  />
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="./carousel/2.jpg"
+                    alt="Medfo"
+                  />
+                </Carousel.Item>
+              </Carousel>
+            </Col>
+          </Row>
+        </Container>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
